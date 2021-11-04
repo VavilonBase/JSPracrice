77227ba7129f2049e42f1f4cc2fdd405e0991754
VavilonBase <VavilonBase@mail.ru>
2021-11-05 01:52:18 +0300

    Realization closable, onClose, onOpen, beforeClose

--- Modal Window Practice/index.js
+++ Modal Window Practice/index.js
@@ -4,5 +4,15 @@ const modal = $.modal({
     <p>Lorem ipsum dolor sit.</p>
     <p>Lorem ipsum dolor sit.</p>
     `,
-    width: '600px'
+    width: '600px',
+    closable: false,
+    onClose: () => {
+        console.log('close')
+    },
+    onOpen: () => {
+        console.log('open')
+    },
+    beforeClose: () => {
+        console.log('beforeClose')
+    }
 })
--- Modal Window Practice/plugins/modal.js
+++ Modal Window Practice/plugins/modal.js
@@ -2,33 +2,57 @@
 
 /*
 * title: string (*)
-* closable: boolean x
+* closable: boolean x (*)
 * content: string (*)
 * width: string (400px) (*)
 * destroy(): void (*)
 * click x or overlay close window (*)
 * ---------------------------------
 * setContent(html: string): void | public (*)
-* onClose(): void
-* onOpen(): void
-* beforeClose(): boolean | true - close else not close
+* onClose(): void (*)
+* onOpen(): void (*)
+* beforeClose(): boolean | true - close else not close (*)
 * -------------------------------------------------------
 * animate.css
 * */
 $.modal = function(options) {
+    const ANIMATION_SPEED = 5000
+    const width = 'width' in options ? options.width : '400px'
+    const title = 'title' in options ? options.title : 'Modal Window'
+    const closable = 'closable' in options ? (!options.closable ? 'hidden' : 'visible') : 'visible'
+    const onClose = 'onClose' in options
+    const onOpen = 'onOpen' in options
+    const beforeClose = 'beforeClose' in options ? options.beforeClose : false
+    let content = 'content' in options ? options.content : 'Content'
     let closing = false
+
+    const $modal = _createModal(options)
+
+    options.beforeClose = () => {
+        beforeClose && beforeClose()
+        return $modal.classList.contains('open')
+    }
+
     function close() {
-        closing = true
-        $modal.classList.remove('open')
-        $modal.classList.add('hide')
-        setTimeout(() => {
-            $modal.classList.remove('hide')
-            closing = false
-        }, ANIMATION_SPEED)
+        if (options.beforeClose()) {
+            closing = true
+            $modal.classList.remove('open')
+            $modal.classList.add('hide')
+            setTimeout(() => {
+                $modal.classList.remove('hide')
+                closing = false
+            }, ANIMATION_SPEED)
+            onClose && options.onClose()
+        }
+    }
+
+    function open() {
+        !closing && $modal.classList.add('open')
+        onOpen && options.onOpen()
     }
 
     function clickOverlay(event) {
-        for (path of event.path) {
+        for (let path of event.path) {
             if (path.className === 'modal-window') {
                 return
             }
@@ -41,13 +65,13 @@ $.modal = function(options) {
         modal.classList.add('amodal')
         modal.insertAdjacentHTML('afterbegin', `
         <div class="modal-overlay">
-            <div class="modal-window" style="width: ${'width' in options ? options.width : '400px'}">
+            <div class="modal-window" style="width: ${width}">
                 <div class="modal-header">
-                    <span class="modal-title">${'title' in options ? options.title : 'Modal Window'}</span>
-                    <span class="modal-close"">&times;</span>
+                    <span class="modal-title">${title}</span>
+                    <span class="modal-close" style="visibility: ${closable}">&times;</span>
                 </div>
                 <div class="modal-body">
-                    ${'content' in options ? options.content : 'Content'}
+                    ${content}
                 </div>
                 <div class="modal-footer">
                     <button>OK</button>
@@ -63,17 +87,16 @@ $.modal = function(options) {
         return modal
     }
 
-    const ANIMATION_SPEED = 200
-    const $modal = _createModal(options)
+
 
     return {
-        open() {
-            !closing && $modal.classList.add('open')
-        },
+        open,
         close,
         setContent(html) {
-            document.querySelector('.modal-body').innerHTML = html
+            content = html
+            document.querySelector('.modal-body').innerHTML = content
         },
+        beforeClose: options.beforeClose,
         destroy() {
             document.querySelector('.modal-close').removeEventListener('click', close)
             document.querySelector('.modal-overlay').removeEventListener('click', clickOverlay)
