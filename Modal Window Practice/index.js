4149f5ed64a9702af3e2e469485943b47ed0829d
VavilonBase <VavilonBase@mail.ru>
2021-11-06 01:42:41 +0300

    create tasks of the youtube

--- Modal Window Practice/index.js
+++ Modal Window Practice/index.js
@@ -1,1 +1,24 @@
-const modal = $.modal()
+const fruits = [
+    {id: 1, title: 'Apple', price: 20, img: 'https://emigracjaposukces.com/media/k2/items/cache/924e149af069b8ea323a809fbb1171d4_M.jpg'},
+    {id: 2, title: 'Orange', price: 30, img: 'https://img.youtube.com/vi/imhj-cR8rb4/0.jpg'},
+    {id: 3, title: 'Mango', price: 40, img: 'https://img.youtube.com/vi/vJk_bjGGUDM/0.jpg'}
+]
+const modal = $.modal({
+    title: 'Artem Modal',
+    closable: true,
+    content: `
+        <p>Modal is working</p>
+        <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '200px',
+    footerButtons: [
+        {text: 'OK', type: 'primary', handler() {
+                console.log('Primary brn clicked')
+                modal.close()
+            }},
+        {text: 'CANCEL', type: 'danger', handler() {
+                console.log('DANGER brn clicked')
+                modal.close()
+            }},
+    ]
+})
--- Modal Window Practice/plugins/modal.js
+++ Modal Window Practice/plugins/modal.js
@@ -1,37 +1,60 @@
+Element.prototype.appendAfter = function(element) {
+    element.parentNode.insertBefore(this, element.nextSibling)
+}
+function noop() {}
+
+function _createModalFooter(buttons = []) {
+    if (buttons.length === 0) {
+        return document.createElement('div')
+    }
+
+    const wrap = document.createElement('div')
+    wrap.classList.add('modal-footer')
+
+    buttons.forEach(btn => {
+        const $btn = document.createElement('button')
+        $btn.textContent = btn.text
+        $btn.classList.add('btn')
+        $btn.classList.add(`btn-${btn.type || 'secondary'}`)
+        $btn.onclick = btn.handler || noop
+        wrap.appendChild($btn)
+    })
+    return wrap
+}
+
 function _createModal(options) {
+    const DEFAULT_WIDTH = '600px'
     const modal = document.createElement('div')
     modal.classList.add('amodal')
     modal.insertAdjacentHTML('afterbegin', `
-        <div class="modal-overlay">
-            <div class="modal-window">
+        <div class="modal-overlay" data-close="true">
+            <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
                 <div class="modal-header">
-                    <span class="modal-title">Modal title</span>
-                    <span class="modal-close">&times;</span>
+                    <span class="modal-title">${options.title || 'Window'}</span>
+                    ${options.closable ? `<span className="modal-close" data-close="true">&times;</span>` : ''}
                 </div>
-                <div class="modal-body">
-                    <p>lorem4</p>
-                    <p>lorem4</p>
+                <div class="modal-body" data-content>
+                    ${options.content || ''}
                 </div>
-                <div class="modal-footer">
-                    <button>OK</button>
-                    <button>Cancel</button>
-                </div>
+                
             </div>
         </div>
     `)
+    const footer = _createModalFooter(options.footerButtons)
+    footer.appendAfter(modal.querySelector('[data-content]'))
     document.body.appendChild(modal)
     return modal
 }
 
 /*
-* title: string
-* closable: boolean x
-* content: string
-* width: string (400px)
-* destroy(): void
+* title: string (*)
+* closable: boolean x (*)
+* content: string (*)
+* width: string (400px) (*)
+* destroy(): void (*)
 * click x or overlay close window
+* setContent(html: string): void | public (*)
 * ---------------------------------
-* setContent(html: string): void | public
 * onClose(): void
 * onOpen(): void
 * beforeClose(): boolean | true - close else not close
@@ -42,8 +65,12 @@ $.modal = function(options) {
     const ANIMATION_SPEED = 200
     const $modal = _createModal(options)
     let closing = false
-    return {
+    let destroyed = false
+    const modal = {
         open() {
+            if (destroyed) {
+                return console.log('Modal is destoryed')
+            }
             !closing && $modal.classList.add('open')
         },
         close() {
@@ -55,7 +82,25 @@ $.modal = function(options) {
                 closing = false
             }, ANIMATION_SPEED)
         },
-        destroy() {}
     }
+
+    const listener = event => {
+        if (event.target.dataset.close) {
+            modal.close()
+        }
+    }
+
+    $modal.addEventListener('click', listener)
+
+    return Object.assign(modal, {
+        destroy() {
+            $modal.parentNode.removeChild($modal)
+            $modal.removeEventListener('click', listener)
+            destroyed = true
+        },
+        setContent(html) {
+            $modal.querySelector('[data-content]').innerHTML = html
+        }
+    })
 }
 
