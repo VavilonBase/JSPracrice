dbb7eeaad31b6f9d422daaa5c7784701b0d45daa
VavilonBase <VavilonBase@mail.ru>
2021-11-13 03:18:58 +0300

    Complete all tasks for youtube tutorial

--- Modal Window Practice/index.js
+++ Modal Window Practice/index.js
@@ -1,24 +1,59 @@
-const fruits = [
+let fruits = [
     {id: 1, title: 'Apple', price: 20, img: 'https://emigracjaposukces.com/media/k2/items/cache/924e149af069b8ea323a809fbb1171d4_M.jpg'},
     {id: 2, title: 'Orange', price: 30, img: 'https://img.youtube.com/vi/imhj-cR8rb4/0.jpg'},
     {id: 3, title: 'Mango', price: 40, img: 'https://img.youtube.com/vi/vJk_bjGGUDM/0.jpg'}
 ]
-const modal = $.modal({
-    title: 'Artem Modal',
+
+const toHTML = fruit => `
+    <div class="col">
+        <div class="card">
+            <img style="height: 300px" src="${fruit.img}" class="card-img-top" alt="${fruit.title}">
+            <div class="card-body">
+                <h5 class="card-title">${fruit.title}</h5>
+                <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Посмотреть цену</a>
+                <a href="#" class="btn btn-danger" data-btn="remove" data-id="${fruit.id}">Удалить</a>
+            </div>
+        </div>
+    </div>
+`
+
+function render() {
+    const html = fruits.map(toHTML).join('')
+    document.querySelector('#fruits').innerHTML = html
+}
+
+render()
+
+const priceModal = $.modal({
+    title: 'Цена на товар',
     closable: true,
-    content: `
-        <p>Modal is working</p>
-        <p>Lorem ipsum dolor sit.</p>
-    `,
     width: '200px',
     footerButtons: [
-        {text: 'OK', type: 'primary', handler() {
-                console.log('Primary brn clicked')
-                modal.close()
-            }},
-        {text: 'CANCEL', type: 'danger', handler() {
-                console.log('DANGER brn clicked')
-                modal.close()
-            }},
+        {text: 'Закрыть', type: 'primary', handler() {
+                priceModal.close()
+            }}
     ]
+})
+
+document.addEventListener('click', event => {
+    event.preventDefault()
+    const btnType = event.target.dataset.btn
+    const id = +event.target.dataset.id
+    const fruit = fruits.find(fruit => fruit.id === id)
+    if (btnType == "price") {
+        priceModal.setContent(`
+            <p>Цена на ${fruit.title}: <b>${fruit.price}$</b></p>
+        `)
+        priceModal.open()
+    } else if (btnType == 'remove') {
+        $.confirm({
+            title: 'Вы уверены?',
+            content: `<p>Вы удаляете фрукт: <b>${fruit.title}</b></p>`
+        }).then(() => {
+            fruits = fruits.filter(fruit => fruit.id !== id)
+            render()
+        }).catch(() => {
+            console.log('Cancel')
+        })
+    }
 })
