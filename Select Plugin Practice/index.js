06ae2d9b1ebc093ed7048fdc56a42e8bf8453393
VavilonBase <VavilonBase@mail.ru>
2021-11-15 02:33:19 +0300

    Complete open, close and select function

--- Select Plugin Practice/index.js
+++ Select Plugin Practice/index.js
@@ -3,6 +3,10 @@ const s = $.select({
     options: [
         {id: 1, title: 'React'},
         {id: 2, title: 'Angular'},
-        {id: 3, title: 'Vue'}
+        {id: 3, title: 'Vue'},
+        {id: 4, title: 'Vue'},
+        {id: 5, title: 'Vue'},
+        {id: 6, title: 'Vue'},
+        {id: 7, title: 'Vue'}
     ]
 })
--- Select Plugin Practice/select.js
+++ Select Plugin Practice/select.js
@@ -2,99 +2,152 @@ $.select = function(options) {
     const _createAselectNode = () => {
         const aselect = document.createElement('div')
         aselect.classList.add('aselect')
+        aselect.dataset.aselect = 'close'
         return aselect
-    }
-
-    const _createAselectText = () => {
-        const aselectText = document.createElement('span')
-        aselectText.classList.add('aselect__input-text')
-        aselectText.textContent = options.options[0].title || ''
-        return aselectText
-    }
-
-    const _createAselectArrow = () => {
-        const aselectArrow = document.createElement('span')
-        aselectArrow.classList.add('aselect__input-arrow')
-        aselectArrow.innerHTML = `&#9660;`
-        return aselectArrow
     }
 
     const _createAselectOption = (option) => {
         const aselectOption = document.createElement('div')
         aselectOption.classList.add('aselect__option')
         aselectOption.textContent = option.title
+        aselectOption.dataset.aselect = 'option'
+        aselectOption.dataset.value = option.id
         return aselectOption
     }
 
     const _createAselectOptions = () => {
         const aselectOptions = options.options.map(option => _createAselectOption(option))
 
-        if (aselectOptions.length != 0) {
-            aselectOptions[0].classList.add('aselect__selected')
-        }
         return aselectOptions
     }
 
     //Aselect node
     const $aselect = _createAselectNode()
-    const $aselectText = _createAselectText()
-    const $aselectArrow = _createAselectArrow()
     const $aselectOptions = _createAselectOptions()
-
+    //Selected option
+    let selected = 0;
     function _createSelect() {
         //Root
         const $root = document.querySelector(options.root)
 
-        //Create select options
-        let aselectOptions = ''
-        options.options.forEach((option, index) => {
-            if (index === 0) {
-                aselectOptions += `<div class="aselect__option aselect__selected">$option.title</div>`
-            } else {
-                aselectOptions += `<div class="aselect__option">$option.title</div>`
-            }
-        })
-
         //Create html
         const html = `
-            <div class="aselect__overlay">
+            <div class="aselect__overlay" data-aselect="overlay">
                 <div class="aselect__body">
-                    <div class="aselect__input">
+                    <div class="aselect__input" data-aselect="input">
+                        <div class="aselect__input-text" data-aselect="input">${options.options[0].title || ''}</div>
+                        <div class="aselect__input-arrow" data-aselect="input">&#9660;</div>
                     </div>
                     <div class="aselect__dropdown">
                     </div>
                 </div>
             </div>
         `
-
+        //Set innerHTML to the aselect
         $aselect.innerHTML = html
 
-        const aselectInput = $aselect.querySelector('.aselect__input')
-        aselectInput.appendChild($aselectText)
-        aselectInput.appendChild($aselectArrow)
-
-        const aselectDropdown = $aselect.querySelector('.aselect__dropdown')
-        $aselectOptions.forEach($aselectOption => {
-            aselectDropdown.appendChild($aselectOption)
-        })
-
+        //Append aselect to the DOM in end of the root
         $root.appendChild($aselect)
 
         return
     }
 
+    //Create select
     _createSelect()
 
+    //Dropdown element
+    const $aselectDropdown = $aselect.querySelector('.aselect__dropdown')
+    //Input text
+    const $aselectInputText = $aselect.querySelector('.aselect__input-text')
+    //Input arrow
+    const $aselectInputArrow = $aselect.querySelector('.aselect__input-arrow')
+    //Function update option, return selected option
+    function _updateOption(newSelect) {
+        let aselectOptionIndex = null
+        $aselectDropdown.innerHTML = ''
+        //Delete class aselect__selected of the previous option and set this class to the new option
+        $aselectOptions.forEach(($aselectOption, index) => {
+            if (+$aselectOption.dataset.value === selected) {
+                $aselectOption.classList.remove('aselect__selected')
+            }
+            if (+$aselectOption.dataset.value === newSelect) {
+                $aselectOption.classList.add('aselect__selected')
+                aselectOptionIndex = index
+            }
+            $aselectDropdown.appendChild($aselectOption)
+        })
+
+        //selected = newSelect
+        selected = newSelect
+
+        return options.options[aselectOptionIndex]
+    }
+
+    function select(id) {
+        let selectedOption = _updateOption(id)
+        $aselectInputText.textContent = selectedOption.title
+        console.log(selectedOption)
+    }
+
+
+    //Update Options
+    if ($aselectOptions.length !== 0) {
+        _updateOption(+$aselectOptions[0].dataset.value)
+    }
+
+
+    //Event Listeners
+    const open = () => {
+        $aselect.classList.add('open')
+    }
+
+    const close = () => {
+        $aselect.classList.remove('open')
+    }
+
+    //Add Event Listener
+    $aselect.addEventListener('click', (event) => {
+        const dataAselect = event.target.dataset.aselect
+
+        const openSelect = () => {
+            $aselect.dataset.aselect = 'open'
+            open()
+            $aselectInputArrow.innerHTML = '&#9650;'
+        }
+
+        const closeSelect = () => {
+            $aselect.dataset.aselect = 'close'
+            close()
+            $aselectInputArrow.innerHTML = '&#9660;'
+        }
+
+        if (dataAselect) {
+            //if click on input, input text or input arrow
+            if (dataAselect === 'input') {
+                if ($aselect.dataset.aselect === 'close') {
+                    openSelect()
+                } else {
+                   closeSelect()
+                }
+            } else if (dataAselect === 'overlay') {
+                //if click on overlay
+                if ($aselect.dataset.aselect === 'open') {
+                    closeSelect()
+                }
+            } else if (dataAselect === 'option') {
+                //if click on option
+                select(+event.target.dataset.value)
+                closeSelect()
+            }
+
+        }
+    })
+
+
     return {
-        open: () => {
-            console.log('Open')
-        },
-        close: () => {
-            console.log('Close')
-        },
-        select: (id) => {
-            console.log(`Selected: ${id}`)
-        },
+        open,
+        close,
+        select,
         destroy: () => {
             console.log('Destroy')
         }
